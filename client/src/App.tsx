106f8961e23f9f6bde4cd3a7d4699526f60a8b6b
Tarasov Nikita <nvisary@gmail.com>
2022-08-06 23:31:48 +0300

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Apptest
+    </div>
+  );
+};
